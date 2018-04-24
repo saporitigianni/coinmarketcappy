8ef02c2d56511fdfbe37041002eb040f74791504
giannisaporiti <giansapo@gmail.com>
2018-04-24 17:22:08 -0400

    added total_market_cap method and other enhancements

--- coinmarketcap/scrape.py
+++ coinmarketcap/scrape.py
@@ -5,89 +5,60 @@ import json
 import re
 from string import whitespace
 
-r2013 = [20130428, 20130505, 20130512, 20130519, 20130526, 20130602,
-         20130609, 20130616, 20130623, 20130630, 20130707, 20130714,
-         20130721, 20130728, 20130804, 20130811, 20130818, 20130825,
-         20130901, 20130908, 20130915, 20130922, 20130929, 20131006,
-         20131013, 20131020, 20131027, 20131103, 20131110, 20131117,
-         20131124, 20131201, 20131208, 20131215, 20131222, 20131229]
-r2014 = [20140105, 20140112, 20140119, 20140126, 20140202, 20140209,
-         20140216, 20140223, 20140302, 20140309, 20140316, 20140323,
-         20140330, 20140406, 20140413, 20140420, 20140427, 20140504,
-         20140511, 20140518, 20140525, 20140601, 20140608, 20140615,
-         20140622, 20140629, 20140706, 20140713, 20140720, 20140727,
-         20140803, 20140810, 20140817, 20140824, 20140831, 20140907,
-         20140914, 20140921, 20140928, 20141005, 20141012, 20141019,
-         20141026, 20141102, 20141109, 20141116, 20141123, 20141130,
-         20141207, 20141214, 20141221, 20141228]
-r2015 = [20150104, 20150111, 20150118, 20150125, 20150201, 20150208,
-         20150215, 20150222, 20150301, 20150308, 20150315, 20150322,
-         20150329, 20150405, 20150412, 20150419, 20150426, 20150503,
-         20150510, 20150517, 20150524, 20150531, 20150607, 20150614,
-         20150621, 20150628, 20150705, 20150712, 20150719, 20150726,
-         20150802, 20150809, 20150816, 20150823, 20150830, 20150906,
-         20150913, 20150920, 20150927, 20151004, 20151011, 20151018,
-         20151025, 20151101, 20151108, 20151115, 20151122, 20151129,
-         20151206, 20151213, 20151220, 20151227]
-r2016 = [20160103, 20160110, 20160117, 20160124, 20160131, 20160207,
-         20160214, 20160221, 20160228, 20160306, 20160313, 20160320,
-         20160327, 20160403, 20160410, 20160417, 20160424, 20160501,
-         20160508, 20160515, 20160522, 20160529, 20160605, 20160612,
-         20160619, 20160626, 20160703, 20160710, 20160717, 20160724,
-         20160731, 20160807, 20160814, 20160821, 20160828, 20160904,
-         20160911, 20160918, 20160925, 20161002, 20161009, 20161016,
-         20161023, 20161030, 20161106, 20161113, 20161120, 20161127,
-         20161204, 20161211, 20161218, 20161225]
-r2017 = [20170101, 20170108, 20170115, 20170122, 20170129, 20170205,
-         20170212, 20170219, 20170226, 20170305, 20170312, 20170319,
-         20170326, 20170402, 20170409, 20170416, 20170423, 20170430,
-         20170507, 20170514, 20170521, 20170528, 20170604, 20170611,
-         20170618, 20170625, 20170702, 20170709, 20170716, 20170723,
-         20170730, 20170806, 20170813, 20170820, 20170827, 20170903,
-         20170910, 20170917, 20170924, 20171001, 20171008, 20171015,
-         20171022, 20171029, 20171105, 20171112, 20171119, 20171126,
-         20171203, 20171210, 20171217, 20171224, 20171231]
-r2018 = [20180107, 20180114, 20180121, 20180128, 20180204, 20180211,
-         20180218, 20180225, 20180304, 20180311, 20180318, 20180325,
-         20180401, 20180408, 20180415]
-default_dates = [*r2013, *r2014, *r2015, *r2016, *r2017, *r2018]
-BASE_URL = 'https://coinmarketcap.com/historical/'
+
+default_dates = 'all'
+SNAPS_URL = 'https://coinmarketcap.com/historical/'
 DOMINANCE_URL = 'https://graphs2.coinmarketcap.com/global/dominance/'
-# Coinmarketcap asks that you don't submit more that 10 requests per minute, hence the 6 second sleep.
+MARKETCAP_URL = 'https://graphs2.coinmarketcap.com/global/marketcap-total/'
+MARKETCAP_ALTCOIN_URL = 'https://graphs2.coinmarketcap.com/global/marketcap-altcoin/'
+# Coinmarketcap asks that you don't submit more that 10 requests per minute, hence the 6 second wait.
 # Remove/reduce if you need to download a large number of dates that would take forever otherwise.
 RATE_LIMIT = 6
 
 
-def retrieve_and_cache(dates=default_dates, out_file=None, cache_file=None, rformat='json', wformat='json'):
+def historical_snapshots(dates=default_dates, out_file=None, cache_file=None, rformat='json', wformat='json',
+                         cache=True, rate_limit=RATE_LIMIT):
     """
     Retrieves all the data from the specified file or coinmarketcap.com and caches it to a local file
     for faster subsequent access times
+    For a list of all available dates refer to https://coinmarketcap.com/historical/
 
+    NOTE: A separate request submitted for each date provided so it might take a while if retrieveing mak=ny dates
+        at once
+
+    :param rate_limit:
     :param dates: dates to retrieve data for. If 'all' is passed in then all dates are fetched from the website first
-    :param out_file: file to write the requested data to
-    :param cache_file: file to check for some or all of the requested dates
+    :param out_file: file to write the requested data to (local file or absolute path to file)
+    :param cache_file: file to check for some or all of the requested dates (local file or absolute path to file)
     :param rformat: format of the cache file to check
     :param wformat: format output file to write to
+    :param cache: If False, the results won't be cached (very slow subsequent access times)
+    :param rate_limit: time to wait between requests to coinmarketcap
     :return: json format data
     """
     if dates == 'all':
         print('Fetching all dates...')
         dates = available_dates()
     # Retrieves data, caches it to a file and reads from it before returning
-    result = retrieve(dates, cache_file, rformat)
-    print('Writing to file...')
-    write_to_file(result, out_file, wformat)
-    result = read_from_file(out_file, wformat)
+    result = _retrieve_snaps(dates, cache_file, rformat, rate_limit)
+    if cache:
+        print('Writing to file...')
+        write_to_file(result, out_file, wformat)
+        result = read_from_file(out_file, wformat)
     return result.copy()
 
 
-def retrieve(dates=default_dates, file=None, rformat=None):
+def _retrieve_snaps(dates=default_dates, file=None, rformat=None, rate_limit=RATE_LIMIT):
     """
     Retrieves data from file if available or coinmarketcap.com
+    For a list of all available dates refer to https://coinmarketcap.com/historical/
 
-    :param dates: dates to retrieve data for. If 'all' is passed in then all dates are fetched from the website first
-    :param file: cache file to check for some or all of the requested dates
+    NOTE: There's a separate request submitted
+
+    :param dates: dates to retrieve data for. If 'all' then all dates are fetched from the website first
+    :param file: cache file to check for some or all of the requested dates (local file or absolute path to file)
     :param rformat: format of the cache file to check
+    :param rate_limit: time to wait between requests to coinmarketcap
     :return: json format data
     """
     missing = list()
@@ -123,7 +94,7 @@ def retrieve(dates=default_dates, file=None, rformat=None):
     for i in range(total_length):
         print('Retrieving {}'.format(dates[i]))
         # Retrieve data from coinmarketcap.com and find all token entries (next 3 lines)
-        response = requests.get(BASE_URL + str(dates[i]))
+        response = requests.get(SNAPS_URL + str(dates[i]))
         soup = bs4.BeautifulSoup(response.content, 'html.parser')
         tr = soup.find_all('tr')
         # Parses the all coins which should be more than enough to find the top 10 PoS (0th entry is dummy)
@@ -138,19 +109,24 @@ def retrieve(dates=default_dates, file=None, rformat=None):
             fetched_data[dates[i]][rank]["symbol"] = symbol
             fetched_data[dates[i]][rank]["name"] = name
             try:  # MARKET CAP CAN BE A QUESTION MARK
-                fetched_data[dates[i]][rank]["market_cap"] = int(td[3].get_text().strip('$' + whitespace).replace(',', ''))
-            except:
-                fetched_data[dates[i]][rank]["market_cap"] = td[3].get_text().strip('$' + whitespace).replace(',', '')
+                fetched_data[dates[i]][rank]["market_cap"] =\
+                    int(td[3].get_text().strip('$' + whitespace).replace(',', ''))
+            except ValueError:
+                fetched_data[dates[i]][rank]["market_cap"] =\
+                    td[3].get_text().strip('$' + whitespace).replace(',', '')
             fetched_data[dates[i]][rank]["price"] = float(td[4].get_text().strip('$' + whitespace).replace(',', ''))
             try:  # CIRCULATING SUPPLY CAN BE A QUESTION MARK
-                fetched_data[dates[i]][rank]["circulating_supply"] = int(td[5].get_text().strip('*' + whitespace).replace(',', ''))
-            except:
-                fetched_data[dates[i]][rank]["circulating_supply"] = td[5].get_text().strip('*' + whitespace).replace(',', '')
+                fetched_data[dates[i]][rank]["circulating_supply"] =\
+                    int(td[5].get_text().strip('*' + whitespace).replace(',', ''))
+            except ValueError:
+                fetched_data[dates[i]][rank]["circulating_supply"] =\
+                    td[5].get_text().strip('*' + whitespace).replace(',', '')
             try:  # 24hr_vol CAN BE THE STRING 'Low Vol'
-                fetched_data[dates[i]][rank]["24hr_vol"] = int(td[6].get_text().strip('$' + whitespace).replace(',', ''))
-            except:
+                fetched_data[dates[i]][rank]["24hr_vol"] =\
+                    int(td[6].get_text().strip('$' + whitespace).replace(',', ''))
+            except ValueError:
                 fetched_data[dates[i]][rank]["24hr_vol"] = td[6].get_text().strip('$' + whitespace).replace(',', '')
-        time.sleep(RATE_LIMIT)
+        time.sleep(rate_limit)
     return fetched_data.copy()
 
 
@@ -159,7 +135,7 @@ def write_to_file(data=None, file=None, wformat='json'):
     Writes json or csv data to file
 
     :param data: json format data to write to file
-    :param file: file to write to
+    :param file: file to write to (local file or absolute path to file)
     :param wformat: format to write to file on
     :return: None
     """
@@ -182,7 +158,7 @@ def read_from_file(file=None, rformat='json'):
     """
     Reads from file and converts to json format if it isn't already
 
-    :param file: file to read from
+    :param file: file to read from (local file or absolute path to file)
     :param rformat: format of the file
     :return: the data retrieved from file
     """
@@ -261,16 +237,12 @@ def csv_to_json(data=None):
     return converted
 
 
-def retrieve_dominance(start=None, end=None, formatted='alt'):
+def start_end(start=None, end=None, url=None):
     """
-    Retrieves the "Percentage of Market Capitalization (Dominance)" chart data from conmarketcap.com
+    Checks to make sure that either start/end are both specified or neither is specified and concatenates
+    them with the provided url
 
-    :param start: starting date to retrieve for, in epoch time
-    :param end: end date to retrieve for, in epoch time
-    :param formatted: either 'alt' or 'raw'. If 'alt' then all alcoins are summed up. If 'raw' then the
-        coinmarketcap format is kept (e.g. bitcoin, ethereum, ripple, ... Others)
-    :return: the retrieved data either as a dictionary in the format {key: list_of_values}
-        or a dictionary in the format {key: dict_of_values}
+    :return: url/start/end/
     """
     # Checks to make sure that either start/end are both specified or neither is specified
     if end is None:
@@ -285,40 +257,96 @@ def retrieve_dominance(start=None, end=None, formatted='alt'):
             dates = start + '/' + end + '/'
     # Concatenates the base dominance url with dates if needed
     if dates is None:
-        url = DOMINANCE_URL
+        final_url = url
     else:
-        url = DOMINANCE_URL + dates
+        final_url = url + dates
 
+    return final_url
+
+
+def dominance(start=None, end=None, formatted='raw', epoch=False):
+    """
+    Retrieves the "Percentage of Market Capitalization (Dominance)" chart data from conmarketcap.com
+
+    :param start: starting date to retrieve for, in epoch time
+    :param end: end date to retrieve for, in epoch time
+    :param formatted: either 'alt' or 'raw'. If 'alt' then all alcoins are summed up. If 'raw' then the
+        coinmarketcap format is kept (e.g. top 10 + others)
+    :param epoch: True if you want the dates returned to be in epoch format, False if you want datetime format
+    :return: the retrieved data as a dictionary in the format {key: list_of_values} where key is
+        bitcoin or altcoins or ethereum, etc and list_of_values is a list of pairs [[date, percent], [date, percent]...]
+    """
+    if type(epoch) != bool:
+        raise ValueError('Please make sure you are using a boolean for the epoch parameter')
+
+    url = start_end(start, end, DOMINANCE_URL)
     response = requests.get(url)
     json_response = response.json()
 
-    # If raw, return as is. If alt, sum all the altcoins and round to 2 decimals
+    # If raw, return as is rounded to 2 decimals
     if formatted == 'raw':
+        if epoch:  # If epoch is True then just round the percent values and don't convert epochs
+            for x in json_response:
+                json_response[x] = [[x[0], round(x[1], 2)] for x in json_response[x]]
+        else:  # If epoch is False then convert epochs and round
+            for x in json_response:
+                json_response[x] = [[epoch_to_date(x[0]), round(x[1], 2)] for x in json_response[x]]
         return json_response
+
+    # If alt, sum all the altcoins and round to 2 decimals
     elif formatted == 'alt':
         result = dict()
-        result['bitcoin'] = {epoch_to_date(x[0]): x[1] for x in json_response['bitcoin'].copy()}
         result['altcoins'] = dict()
-        for entry in json_response:
-            if entry == 'bitcoin':
-                continue
+        btc_temp = json_response['bitcoin'].copy()
+        del json_response['bitcoin']  # Remove btc so that it is not included in calculations or made into a dict
+
+        for entry in json_response:  # Go through each entry and date for altcoins and add up marketcap percentages
             for date in json_response[entry]:
-                date0 = epoch_to_date(date[0])
-                if date0 not in result['altcoins']:
-                    result['altcoins'][date0] = 0
-                result['altcoins'][date0] += date[1]
-        for x in result:
-            for y in result[x]:
-                result[x][y] = round(result[x][y], 2)
+                if date[0] not in result['altcoins']:
+                    result['altcoins'][date[0]] = 0
+                result['altcoins'][date[0]] += date[1]
+
+        if epoch:  # Round precentages and convert epochs if necessary
+            for x in result:
+                result[x] = [[y, round(result[x][y], 2)] for y in sorted(result[x].keys())]
+            result['bitcoin'] = [[x[0], round(x[1], 2)] for x in btc_temp]
+        else:
+            for x in result:
+                result[x] = [[epoch_to_date(y), round(result[x][y], 2)] for y in sorted(result[x].keys())]
+            result['bitcoin'] = [[epoch_to_date(x[0]), round(x[1], 2)] for x in btc_temp]
+        return result
     else:
         raise ValueError('Please enter a valid return format. Valid options are "raw" or "alt"')
-    return result
+
+
+def total_market_cap(start=None, end=None, exclude_btc=False, epoch=False):
+    """
+    Retrieves the "Total Market Capitalization" chart data from conmarketcap.com (option to exclude bitcoin)
+
+    :param start: starting date to retrieve for, in epoch time
+    :param end: end date to retrieve for, in epoch time
+    :param exclude_btc: if True, the "Total Market Capitalization (Excluding Bitcoin)" is scraped instead
+    :param epoch: True if you want the dates returned to be in epoch format, False if you want datetime format
+    :return: the retrieved data either as a dictionary in the format {key: list_of_values}
+        or a dictionary in the format {key: dict_of_values}
+    """
+    if (type(epoch) != bool) or (type(exclude_btc) != bool):
+        raise ValueError('Please make sure you are using a boolean for the epoch and exclude_btc parameters')
+
+    url = start_end(start, end, MARKETCAP_ALTCOIN_URL if exclude_btc else MARKETCAP_URL)
+    response = requests.get(url)
+    json_response = response.json()
+
+    if epoch:
+        return json_response['market_cap_by_available_supply']
+    else:
+        return [[epoch_to_date(x[0]), x[1]] for x in json_response['market_cap_by_available_supply']]
 
 
 def epoch_to_date(date=None):
     if date is None:
         ValueError('Please enter an epoch time to convert to date')
-    return time.strftime('%Y-%m-%d', time.localtime(date/1000))
+    return time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(date/1000))
 
 
 def available_dates():
@@ -328,7 +356,7 @@ def available_dates():
     :return: a list of string dates in ascending order
     """
     dates = set()
-    response = requests.get(BASE_URL)
+    response = requests.get(SNAPS_URL)
     soup = bs4.BeautifulSoup(response.content, 'html.parser')
     ul = soup.find_all('ul')
     for entry in ul:
@@ -342,23 +370,4 @@ def available_dates():
                     dates.add(match.group(1))
     return sorted(list(dates))
 
-
-if __name__ == '__main__':
-    # temp = retrieve_and_cache('all', 'historical_20180415.json', 'historical_20180415.json',
-    #                           rformat='json', wformat='json')
-    # temp = read_from_file('csv_historical_20180408.csv', rformat='csv')
-    # print(temp)
-    # print(available_dates())
-    dominance = retrieve_dominance()
-    print(list(dominance['bitcoin'].keys()))
-    print(list(dominance['bitcoin'].values()))
-    print(list(dominance['altcoins'].values()))
-    # for x in dominance:
-    #     print(x, dominance[x])
-    # print(retrieve_dominance())
-    # for x in dominance['altcoins'].keys():
-    #     print(x)
-    # for a in range(len(temp)):
-    #     print(str(default_dates[a]), isinstance(default_dates[a], int))
-    #     for b in temp[str(default_dates[a])]:
-    #         print('\t', b, ' : ', temp[str(default_dates[a])][b])
+# TODO add saving ability for dominance and total_market_cap
---
+++ coinmarketcap/test/test_scrape.py
@@ -0,0 +1,24 @@
+from coinmarketcap import scrape
+
+
+temp = scrape.historical_snapshots('all',
+                                   'historical_snaps.json',
+                                   None,
+                                   rformat='json', wformat='json')
+dominance = scrape.dominance(formatted='raw', epoch=True)
+for x in dominance:
+    print(x, dominance[x])
+
+dominance = scrape.dominance(formatted='alt', epoch=False)
+for x in dominance:
+    print(x, dominance[x])
+
+total_mc = scrape.total_market_cap(exclude_btc=False, epoch=False)
+print(total_mc)
+
+total_mc = scrape.total_market_cap(exclude_btc=True, epoch=True)
+print(total_mc)
+
+# print(list(dominance['bitcoin'].keys()))
+# print(list(dominance['bitcoin'].values()))
+# print(list(dominance['altcoins'].values()))
